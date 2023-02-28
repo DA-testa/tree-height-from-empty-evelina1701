5215308361213232d6d2a616d46993db08e512a9
Evelīna Geikina <118215390+evelina1701@users.noreply.github.com>
2023-02-28 10:41:55 +0000

    -

--- tree_height.py
+++ tree_height.py
@@ -35,7 +35,7 @@ def main():
         if "a" in fileName:
             print("Enter a file name without letter 'a'")
             return
-        with open(f"tree-height-from-empty-evelina1701/test/{fileName}.txt", "r") as file:
+        with open(f"test/{fileName}.txt", "r") as file:
             number = int(file.readline().strip())
             values = list(map(int, file.readLine().strip().split())) 
     # let user input file name to use, don't allow file names with letter a
